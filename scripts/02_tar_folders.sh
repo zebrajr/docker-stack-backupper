842383518754f2f3f7a57194e257ea023b7c0ee9
Carlos Sousa <me@carlossousa.tech>
2024-06-03 11:52:34 +0200

    update tar to keep permissions

--- scripts/02_tar_folders.sh
+++ scripts/02_tar_folders.sh
@@ -7,6 +7,6 @@ mkdir -p /home/${REMOTE_USER}/${REMOTE_PATH}/data/${current_date}
 # Tar.gz each folder in the target directory
 for dir in ${REMOTE_DIRECTORY_BACKUP}/*/; do
     echo "Backing Up: ${dir}"
-    sudo tar -czf "/home/${REMOTE_USER}/${REMOTE_PATH}/data/${current_date}/$(basename "$dir").tar.gz" -C "$dir" .
+    sudo tar -czpf "/home/${REMOTE_USER}/${REMOTE_PATH}/data/${current_date}/$(basename "$dir").tar.gz" -C "$dir" .
     sudo chown ${REMOTE_USER}:${REMOTE_USER} "/home/${REMOTE_USER}/${REMOTE_PATH}/data/${current_date}/$(basename "$dir").tar.gz"
 done
